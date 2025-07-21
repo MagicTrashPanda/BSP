7b07f9ef3a8c5c1ec594a90d3e81cebf3825a45c
Lucas R <cmc5039@gmail.com>
2025-07-20 20:16:49 -0400

    Fixed rotation but actually

--- Runtime/GeneralScripts/AlwaysUpSprite.cs
+++ Runtime/GeneralScripts/AlwaysUpSprite.cs
@@ -9,7 +9,7 @@ public class AlwaysUpSprite : MonoBehaviour
     void Update()
     {
         Vector3 currentRotation = transform.eulerAngles;
-        currentRotation.y = 0f; // Lock Y rotation to 0
+        currentRotation.x = 0f; // Lock X rotation to 0
         currentRotation.z = 0f; // Lock Z rotation to 0
         transform.eulerAngles = currentRotation;
     }
